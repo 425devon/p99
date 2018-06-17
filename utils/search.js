dad9efcbc6d3f2d21a180bb1ea828aff14fe9ec2
425devon <425devon@gmail.com>
2018-06-17 09:30:31 -0700

    cleaned up error handling

--- utils/search.js
+++ utils/search.js
@@ -1,13 +1,10 @@
 const axios = require('axios');
 
-module.exports = async (itemName) =>{
+module.exports = async (item) =>{
     try {
-        const item = itemName;
         const response = await axios.get(`https://p99market.com/api/search/${item}`);
-        //console.log(response.data.data.items);
         return response.data.data.items;
       } catch (error) {
-        console.error("here?");
+        console.error(error);
       }
-
 }
