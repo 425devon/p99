cf076225d93f3ca2f32712a76678e40925057013
425devon <425devon@gmail.com>
2018-06-17 09:26:55 -0700

    added option support for auctionBuilder

--- cmnds/search.js
+++ cmnds/search.js
@@ -1,19 +1,28 @@
 const ora = require('ora')
 const getSearch = require('../utils/search')
+const itemTableBuilder = require('../utils/itemTableBuilder')
+const auctionTableBuilder = require('../utils/auctionTableBuilder')
 
 module.exports = async (args) => {
   const spinner = ora().start()
 
+
   try {
     const item = args._[1];
     const search = await getSearch(item)
+    const limit = args.limit || args.l || 0;
 
+
+    if(search.length == 0 || !search){
+        console.log(`${item} was not found`)
+    }else{
+        if(args.auction || args.a){
+            setTimeout(()=> auctionTableBuilder(search, limit), 500)
+        }
+       itemTableBuilder(search)
+    }
     spinner.stop()
-
-    console.log(search)
   } catch (err) {
-    spinner.stop()
-
     console.error(err)
   }
 }
