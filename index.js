93c74aff1dfb8547c074f205b990ca9c1a7dc523
425devon <425devon@gmail.com>
2018-06-16 20:46:22 -0700

    added help and version cases && data cleaning for search

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const minimist = require('minimist')
+const minimist = require('minimist');
 
 module.exports = () => {
     console.log(`
@@ -12,19 +12,39 @@ module.exports = () => {
   $$  ____/ \\______/ \\______/\\__|     \\__|\\_______\\__|     \\__|  \\__|\\_______| \\____/ 
   $$ |                                                                                
   $$ |                                                                                
-  \\__|     
-                                                   
-    `)
-    const args = minimist(process.argv.slice(2))
-    const cmd = args._[0]
-    //console.log(cmd)
+  \\__|                                                      
+    `);
+    const args = minimist(process.argv.slice(2));
+    let cmd = args._[0] || 'help';
+
+    //Cleaning for search terms with multiple words
+    if(args._.length > 2){
+        let wordsArray = args._.slice(1)
+        cmd = wordsArray.join('_')
+    }
+    
+
+    if (args.version || args.v) {
+        cmd = 'version'
+    }
+
+    if (args.help || args.h) {
+    cmd = 'help'
+    }
+    
 
     switch (cmd) {
         case 'search':
         require('./cmnds/search')(args)
-        break
+        break;
+        case 'help':
+        require('./cmnds/help')(args)
+        break;
+        case 'version':
+        require('./cmnds/version')(args)
+        break;
         default:
         console.error(`"${cmd}" is not a valid command!`)
-        break
+        break;
     }
 }
