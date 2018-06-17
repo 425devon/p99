d333ca05e607a6d1c7f7c4ae554c87a0125dde5e
425devon <425devon@gmail.com>
2018-06-17 01:53:19 -0700

    implemented dynamic table creation

---
+++ utils/itemTableBuilder.js
@@ -0,0 +1,46 @@
+const ora = require('ora')
+const itemData = require('./itemData')
+const Table = require('cli-table');
+
+module.exports = async (itemArray) =>{
+    const spinner = ora().start();
+    // let table = new Table({
+    //     head: ['Item Name', 'Type', 'Slot', 'AC', 'STA', 'INT', 'WT', 'Size', 'Class', 'Race']
+    //   , colWidths: [25,25,10,5,5,5]
+    // });
+
+    const getData = async (items) => {
+        let objArray = [];
+        for (let i = 0; i < items.length; i++) {
+          const result = await itemData(items[i]._id)
+          objArray.push(result);
+        }
+        tableBuilder(objArray)
+        spinner.stop();
+    }
+
+    const tableBuilder = (data) =>{
+        // spinner.start();
+        for(let obj in data){
+            let tableConstructor = {head:[], colWidths:[]};
+            let tableData = [];
+
+            for(let key in data[obj]){
+                tableConstructor.head.push(key)
+                if(key == 'NAME' || key == 'TYPE' || key == 'CLASS' || key == 'RACE'){
+                    tableConstructor.colWidths.push(25)
+                }else if(key == 'SLOT' || key == 'SIZE'){
+                    tableConstructor.colWidths.push(10)
+                }else{
+                    tableConstructor.colWidths.push(5)
+                }
+                tableData.push(data[obj][key])
+            }
+            let table = new Table(tableConstructor);
+            table.push(tableData)
+            spinner.stop();
+            console.log(table.toString());
+        }
+    }
+    getData(itemArray);
+}
