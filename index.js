1171665e72b85f9d72516c025e81e3a9a89acd30
425devon <425devon@gmail.com>
2018-06-21 22:50:48 -0700

    a big mess....

--- index.js
+++ index.js
@@ -1,52 +1,59 @@
 const minimist = require('minimist');
-const allItems = require('./static/allItems');
+const test = require('./test');
 
 module.exports = () => {
-    console.log(`
-            $$$$$$\\  $$$$$$\\ $$\\      $$\\                  $$\\                $$\\     
-           $$  __$$\\$$  __$$\\$$$\\    $$$ |                 $$ |               $$ |    
-   $$$$$$\\ $$ /  $$ $$ /  $$ $$$$\\  $$$$ |$$$$$$\\  $$$$$$\\ $$ |  $$\\ $$$$$$\\$$$$$$\\   
-  $$  __$$\\\\$$$$$$$ \\$$$$$$$ $$\\$$\\$$ $$ |\\____$$\\$$  __$$\\$$ | $$  $$  __$$\\_$$  _|  
-  $$ /  $$ |\\____$$ |\\____$$ $$ \\$$$  $$ |$$$$$$$ $$ |  \\__$$$$$$  /$$$$$$$$ |$$ |    
-  $$ |  $$ $$\\   $$ $$\\   $$ $$ |\\$  /$$ $$  __$$ $$ |     $$  _$$< $$   ____|$$ |$$\\ 
-  $$$$$$$  \\$$$$$$  \\$$$$$$  $$ | \\_/ $$ \\$$$$$$$ $$ |     $$ | \\$$\\\\$$$$$$$\\ \\$$$$  |
-  $$  ____/ \\______/ \\______/\\__|     \\__|\\_______\\__|     \\__|  \\__|\\_______| \\____/ 
-  $$ |                                                                                
-  $$ |                                                                                
-  \\__|                                                      
-    `);
+//     console.log(`
+//             $$$$$$\\  $$$$$$\\ $$\\      $$\\                  $$\\                $$\\     
+//            $$  __$$\\$$  __$$\\$$$\\    $$$ |                 $$ |               $$ |    
+//    $$$$$$\\ $$ /  $$ $$ /  $$ $$$$\\  $$$$ |$$$$$$\\  $$$$$$\\ $$ |  $$\\ $$$$$$\\$$$$$$\\   
+//   $$  __$$\\\\$$$$$$$ \\$$$$$$$ $$\\$$\\$$ $$ |\\____$$\\$$  __$$\\$$ | $$  $$  __$$\\_$$  _|  
+//   $$ /  $$ |\\____$$ |\\____$$ $$ \\$$$  $$ |$$$$$$$ $$ |  \\__$$$$$$  /$$$$$$$$ |$$ |    
+//   $$ |  $$ $$\\   $$ $$\\   $$ $$ |\\$  /$$ $$  __$$ $$ |     $$  _$$< $$   ____|$$ |$$\\ 
+//   $$$$$$$  \\$$$$$$  \\$$$$$$  $$ | \\_/ $$ \\$$$$$$$ $$ |     $$ | \\$$\\\\$$$$$$$\\ \\$$$$  |
+//   $$  ____/ \\______/ \\______/\\__|     \\__|\\_______\\__|     \\__|  \\__|\\_______| \\____/ 
+//   $$ |                                                                                
+//   $$ |                                                                                
+//   \\__|                                                      
+//     `);
     
-    let args = minimist(process.argv.slice(2));
-    let cmd = args._[0] || 'help';
+//     let args = minimist(process.argv.slice(2));
+//     let cmd = args._[0] || 'help';
 
-    //Cleaning for search terms with multiple words
-    if(args._[0] == 'search' && args._.length > 2){
-        let itemName = args._.slice(1).join('_');
-        args = { _: [ 'search', itemName ] }
-    }
-    
-
-    if (args.version || args.v) {
-        cmd = 'version'
-    }
-
-    if (args.help || args.h) {
-    cmd = 'help'
-    }
+//     //Cleaning for search terms with multiple words
+//     if(args._[0] == 'search' && args._.length > 2){
+//         let itemName = args._.slice(1).join('_');
+//         args = { _: [ 'search', itemName ] }
+//     }
     
 
-    switch (cmd) {
-        case 'search':
-        require('./cmnds/search')(args)
-        break;
-        case 'help':
-        require('./cmnds/help')(args)
-        break;
-        case 'version':
-        require('./cmnds/version')(args)
-        break;
-        default:
-        console.error(`"${cmd}" is not a valid command!`)
-        break;
+//     if (args.version || args.v) {
+//         cmd = 'version'
+//     }
+
+//     if (args.help || args.h) {
+//     cmd = 'help'
+//     }
+    
+
+//     switch (cmd) {
+//         case 'search':
+//         require('./cmnds/search')(args)
+//         break;
+//         case 'help':
+//         require('./cmnds/help')(args)
+//         break;
+//         case 'version':
+//         require('./cmnds/version')(args)
+//         break;
+//         default:
+//         console.error(`"${cmd}" is not a valid command!`)
+//         break;
+//     }
+
+    const testSearch = async () =>{
+        const item = await test();
+        console.log(item);
     }
+
+    testSearch();
 }
---
+++ test.js
@@ -0,0 +1,40 @@
+const inquirer = require('inquirer');
+const _ = require('lodash');
+const fuzzy = require('fuzzy');
+const Promise = require('promise');
+const allItems = require('./static/allItems');
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+function searchItem(answers, input) {
+    input = input || '';
+    return new Promise(function(resolve) {
+      setTimeout(function() {
+        var fuzzyResult = fuzzy.filter(input, allItems.itemNames);
+        resolve(fuzzyResult.map(function(el) {
+          return el.original;
+        }));
+      }, _.random(30, 500));
+    });
+  }
+  
+  inquirer.prompt([
+    {
+      type: 'autocomplete',
+      name: 'item',
+      suggestOnly: true,
+      message: 'What are you searching for?',
+      source: searchItem,
+      pageSize: 3,
+      validate: function(val) {
+        return val
+          ? true
+          : 'Type something!';
+      }
+    }
+  ]).then(function(answers) {
+    console.log(JSON.stringify(answers, null, 2));
+  });
+  
+
+
+
