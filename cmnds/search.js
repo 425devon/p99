2fee6042dda02e6f80ae1fb551366e5c53e2672a
425devon <425devon@gmail.com>
2018-06-23 17:13:24 -0700

    stripped args

--- cmnds/search.js
+++ cmnds/search.js
@@ -3,26 +3,24 @@ const getSearch = require('../utils/search')
 const itemTableBuilder = require('../utils/itemTableBuilder')
 const auctionTableBuilder = require('../utils/auctionTableBuilder')
 
-module.exports = async (args) => {
+module.exports = async (item) => {
   const spinner = ora().start()
 
 
   try {
-    const item = args._[1];
     const search = await getSearch(item)
-    const limit = args.limit || args.l || 0;
-
 
     if(search.length == 0 || !search){
         console.log(`${item} was not found`)
     }else{
-        if(args.auction || args.a){
-            setTimeout(()=> auctionTableBuilder(search, limit), 500)
-        }
+        // if(args.auction || args.a){
+        //     setTimeout(()=> auctionTableBuilder(search, limit), 500)
+        // }
        itemTableBuilder(search)
     }
     spinner.stop()
   } catch (err) {
     console.error(err)
+    process.exit(1);
   }
 }
