70080020157073bf3a1130c1a2be342786e4c497
425devon <425devon@gmail.com>
2018-06-25 20:23:22 -0700

    added efault cae to switch statement

--- utils/auctionHandler.js
+++ utils/auctionHandler.js
@@ -21,6 +21,9 @@ module.exports = async (itemArray) => {
                     case 'nvm...':
                     menu()
                     break;
+                    default:
+                    menu()
+                    break;
                 }
             })
         }else{
