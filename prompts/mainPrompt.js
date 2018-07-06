9e3588a49b2021d100ebd9c2b305ad9480634c42
425devon <425devon@gmail.com>
2018-07-05 19:22:23 -0700

    changed rawlist to list type

--- prompts/mainPrompt.js
+++ prompts/mainPrompt.js
@@ -6,7 +6,7 @@ module.exports ={
     const questions = [
         {
             name: 'menuChoice',
-            type: 'rawlist',
+            type: 'list',
             message: `Please choose from the following:`,
             choices:['Search for item','Search auctions by seller','See version','Get help', 'Exit'],
             default: 0,
