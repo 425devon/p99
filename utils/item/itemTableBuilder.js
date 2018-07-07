6b7bdaa162853e827c5071e94f4800e798fdee73
425devon <425devon@gmail.com>
2018-07-07 11:07:35 -0700

    added sub-directory for item utils

---
+++ utils/item/itemTableBuilder.js
@@ -0,0 +1,38 @@
+const ora = require('ora');
+const itemData = require('./itemData');
+const Table = require('cli-table');
+const auctionHandler = require('../auction/auctionHandler')
+
+module.exports = async (itemArray) =>{
+    const spinner = ora().start();
+    const getData = async (items) => {
+        const result = await itemData(items[0]._id)
+        tableBuilder(result)
+        spinner.stop();
+    }
+    //Todo change to vertical tables
+    //to reduce sizing wrap issues
+    const tableBuilder = (data) =>{
+        let tableConstructor = {head:[], colWidths:[]};
+        let tableData = [];
+
+        for(let key in data){
+            tableConstructor.head.push(key)
+            if(key == 'NAME' || key == 'TYPE' || key == 'CLASS' || key == 'RACE'){
+                tableConstructor.colWidths.push(25)
+            }else if(key == 'SLOT' || key == 'SIZE'){
+                tableConstructor.colWidths.push(10)
+            }else{
+                tableConstructor.colWidths.push(5)
+            }
+            tableData.push(data[key])
+        }
+        let table = new Table(tableConstructor);
+        table.push(tableData)
+        spinner.stop();
+        console.log(table.toString());
+        auctionHandler(itemArray);
+    }
+    getData(itemArray);
+}
+
