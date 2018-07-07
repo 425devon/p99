01cc4258693917564eba433e32605bd891f098b1
425devon <425devon@gmail.com>
2018-07-07 11:07:03 -0700

    added sub-directory for auction utils

---
+++ utils/auction/auctionHandler.js
@@ -0,0 +1,33 @@
+const auction = require('../../prompts/auctionPrompts');
+const auctionData = require('./auctionTableBuilder');
+const menu = require('../../prompts/promptHandler');
+
+module.exports = async (itemArray) => {
+    auction.seeAuction().then((answer)=>{ 
+        if(answer.seeAuction){
+            auction.getAuction().then((res)=>{
+                let cmd = res.getAuction;
+
+                switch(cmd){
+                    case 'Last 5':
+                    auctionData(itemArray,5)
+                    break;
+                    case 'Last 15':
+                    auctionData(itemArray,15)
+                    break;
+                    case 'All data':
+                    auctionData(itemArray,0)
+                    break;
+                    case 'nvm...':
+                    menu()
+                    break;
+                    default:
+                    menu()
+                    break;
+                }
+            })
+        }else{
+            menu();
+        }
+    });
+}
