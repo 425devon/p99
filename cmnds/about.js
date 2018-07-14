e6d8d8120f6db8b534e6a2d0f68e2fd873db1ae0
425devon <425devon@gmail.com>
2018-07-14 15:22:16 -0700

    changed from dev name

--- cmnds/about.js
+++ cmnds/about.js
@@ -1,7 +1,7 @@
 const menu = require('../prompts/promptHandler');
 module.exports =()=>{
     console.log(
-        `\n\tWelcome to c99 the official CLI for www.p99market.com\n
+        `\n\tWelcome to p99 the official CLI for www.p99market.com\n
         You can find all documentation at https://github.com/425devon/c99.git \n`
     );
     menu();
