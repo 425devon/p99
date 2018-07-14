e3c832c69d9c03abb7d73e5129e42d15682ec784
425devon <425devon@gmail.com>
2018-07-14 11:39:40 -0700

    updated about menu

---
+++ cmnds/about.js
@@ -0,0 +1,8 @@
+const menu = require('../prompts/promptHandler');
+module.exports =()=>{
+    console.log(
+        `\n\tWelcome to c99 the official CLI for www.p99market.com\n
+        You can find all documentation at https://github.com/425devon/c99.git \n`
+    );
+    menu();
+}
