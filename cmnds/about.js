056825bb27d56ced0fc7c3704c9f2445d1066d0f
425devon <425devon@gmail.com>
2018-07-14 15:24:19 -0700

    minor naming fixt

--- cmnds/about.js
+++ cmnds/about.js
@@ -2,7 +2,7 @@ const menu = require('../prompts/promptHandler');
 module.exports =()=>{
     console.log(
         `\n\tWelcome to p99 the official CLI for www.p99market.com\n
-        You can find all documentation at https://github.com/425devon/c99.git \n`
+        You can find all documentation at https://github.com/425devon/p99.git \n`
     );
     menu();
 }
